53b8da4d29ef5c60b037bd4d8c97ec2e9ff8ae89
Umesh-Deogharia <umeshdeogharia07@gmail.com>
2025-04-17 23:30:02 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
-
+  // let checked = false;
+  const [isChecked, setIsChecked] = useState(false);
+  function handleClick() {
+    setIsChecked(!isChecked);
+  }
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      <p id="para" className={isChecked ? "show" : "hide"}>
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+      <button id="click" onClick={handleClick}>Click</button>
     </div>
   );
 }
